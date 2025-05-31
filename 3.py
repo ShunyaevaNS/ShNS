a6b61ad9109ef5f150f106b0a6a3590f15f2add9
ShunyaevaNS <ns_shunyaeva@mail.ru>
2025-05-31 10:13:05 +0300

    3.py

--- 3.py
+++ 3.py
@@ -5,7 +5,5 @@ G = nx.erdos_renyi_graph(number_of_nodes, random_edge_percent)
 a = 0
 for n in G.nodes():
     a = a + G.degree(n)
-print(f'Cредняя степень вершины: {float(a) / len(G.nodes())}')
-print(
-    f'Cредняя степень вершины по представленной формуле: '
-    f'{(number_of_nodes - 1) * random_edge_percent}')
+print(f"Средняя степень вершины, вычисляемая встроенной функцией {float(a) / len(G.nodes())}")
+print(f"Cредняя степень вершины по формуле из лекции {(30 - 1) * 0.75}")
